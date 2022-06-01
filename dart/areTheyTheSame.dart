e4459787aba65530eb848c5eac50a9face72b57e
r-fine <71561853+r-fine@users.noreply.github.com>
2022-06-01 09:56:31 +0600

    Update areTheyTheSame.dart

--- dart/areTheyTheSame.dart
+++ dart/areTheyTheSame.dart
@@ -1,16 +1,16 @@
-import "dart:math";
-
 bool comp(List<int> a1, List<int> a2) {
   int c = 0;
   if (a1.length == a2.length) {
     for (int i in a1) {
-      if (a2.contains(pow(i, 2))) {
-        a2.remove(pow(i, 2));
-        c++;
-      } else {
-        return false;
-      }
+    int _i = i*i;
+    if (a2.contains(_i)) {
+      a2.remove(_i);
+      c++;
     }
+    else {
+      return false;
+    }
+  }
   }
   if (c == a1.length) {
     return true;
